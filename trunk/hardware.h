98614687ee7d3b7543a709c4ab9159299b639bb9
bson <bson@c2399955-cda7-47f2-86dd-86c223bbeabf>
2009-05-24 23:26:27 +0000

    remove last vestiges of dependency on lpc22xx.h

--- trunk/hardware.h
+++ trunk/hardware.h
@@ -3,7 +3,53 @@
 
 #include "assert.h"
 
- #include "lpc22xx.h"
+// Memory Accelerator Module (MAM)
+#define MAMCR (*((volatile unsigned char *)0xe01fc000))
+#define MAMTIM (*((volatile unsigned char *)0xe01fc004))
+
+// Memory map control
+#define MEMMAP (*((volatile unsigned char *)0xe01fc040))
+
+// Phase Locked Loop (PLL)
+#define PLLCON (*((volatile unsigned char *)0xe01fc080))
+#define PLLCFG (*((volatile unsigned char *)0xe01fc084))
+#define PLLSTAT (*((volatile unsigned short*)0xe01fc088))
+#define PLLFEED (*((volatile unsigned char *)0xe01fc08c))
+
+// VPB Divider
+#define VPBDIV (*((volatile unsigned char *)0xe01fc100))
+
+// Power Control
+#define PCON (*((volatile unsigned char *)0xe01fc0c0))
+#define PCONP (*((volatile unsigned long *)0xe01fc0c4))
+
+// External interrupts
+#define EXTINT (*((volatile unsigned char*)0xe01fc140))
+#define EXTWAKE (*((volatile unsigned char*)0xe01fc144))
+#define EXTMODE (*((volatile unsigned char*)0xe01fc148))
+#define EXTPOLAR (*((volatile unsigned char*)0xe01fc14c))
+
+// Watchdog
+#define WDMOD (*((volatile unsigned char *)0xe0000000))
+#define WDTC (*((volatile unsigned long *)0xe0000004))
+#define WDFEED (*((volatile unsigned char *)0xe0000008))
+#define WDTV (*((volatile unsigned long *)0xe000000c))
+
+
+// EMC
+#define BCFG0 (*((volatile unsigned long*)0xffe00000))
+#define BCFG1 (*((volatile unsigned long*)0xffe00004))
+#define BCFG2 (*((volatile unsigned long*)0xffe00008))
+#define BCFG3 (*((volatile unsigned long*)0xffe0000c))
+
+// ADC
+#define ADCR (*((volatile unsigned long*)0xe0034000))
+#define ADDR (*((volatile unsigned long*)0xe0034004))
+
+// Pin Connect Block
+#define PINSEL0 (*((volatile unsigned long *)0xe002c000))
+#define PINSEL1 (*((volatile unsigned long *)0xe002c004))
+#define	PINSEL2 (*((volatile unsigned long *)0xe002c014))
 
 __force_inline uint DisableInterrupts() {
 	uint prev;
