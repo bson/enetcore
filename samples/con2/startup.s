8f78be3bbfe1e873e9ef71b7b8eac395145bef62
Jan Brittenson <bson@rockgarden.net>
2021-10-17 15:00:15 -0700

    set up FP

--- samples/con2/startup.s
+++ samples/con2/startup.s
@@ -2,7 +2,7 @@
 @@@
 @@@ Copyright (c) 2018-2021 Jan Brittenson
 @@@ See LICENSE for details.
-	
+
 .syntax  unified
 .section .startup
 .force_thumb
@@ -21,10 +21,10 @@ _startup:
 .weakref SysTick_Handler, _unimpl_vect
 .weakref SVCall_Handler, _unimpl_vect
 
-	. = 0
+    . = 0
     
 _vectors:_vectors:
-	.word   __stack_top         /* Initial SP */
+    .word   __stack_top         /* Initial SP */
     .word   _reset+1            /* Reset vector */
     .word   NMI_Handler+1      /* NMI */
     .word   HardFault_Handler+1 /* Hard fault */
@@ -42,29 +42,29 @@ _vectors:_vectors:
     .word   SysTick_Handler+1  /* SysTick */
 
     /* IRQs */
-	.rept   (0x400-0x40)/4
+    .rept   (0x400-0x40)/4
     .word   _unimpl_vect+1
     .endr
 
     . = 0x400
 .thumb_func
 _reset:
-	/* turn off interrupts */
+    /* turn off interrupts */
     mov      r0, #1
     msr      primask, r0
 
-	/* delay loop */
-	.ifdef DELAY
+    /* delay loop */
+    .ifdef DELAY
     ldr     r0, =DELAY
 3:  subs    r0, r0, #1
     bne     3b
-	.endif
+    .endif
 
-	/* start main stack at top of internal RAM */
+    /* start main stack at top of internal RAM */
     ldr     r0, =__stack_top
     mov     sp, r0
-	            
-	/* copy .data section (Copy from ROM to RAM) */
+
+    /* copy .data section (Copy from ROM to RAM) */
     ldr     r1, =_etext
     ldr     r2, =_data
     ldr     r3, =_edata
@@ -74,31 +74,32 @@ _reset:
     strlo   r0, [r2], #4
     blo     1b
 
-	/* Clear .bss section (Zero init)  */
+    /* Clear .bss section (Zero init)  */
     mov     r0, #0
     ldr     r1, =_bss_start
     ldr     r2, =_bss_end
 2:
-	cmp     r1, r2
+    cmp     r1, r2
     strlo   r0, [r1], #4
     blo     2b
-			
-	/* Create dummy frame */
-	mov     r7, r0
-	mov     lr, r0
+
+    /* Create dummy frame */
+    mov     r7, r0
+    mov     lr, r0
     push    {r7,lr}
-	
-	bl      _Z7hwinit0v
+
+    mov     r11, #0  /* FP=0 so GDB can see where the stack ends */
+    bl      _Z7hwinit0v
     bl      _Z10init_arrayv
-	bl		_Z6hwinitv
+    bl      _Z6hwinitv
     bl      main
-	b		.		/* Wait for watchdog reset, if enabled */
+    b       .       /* Wait for watchdog reset, if enabled */
 
 .align
 .thumb_func
 
 _unimpl_vect:
-	.ifdef DEBUG
+    .ifdef DEBUG
     bkpt #1
     .else
     b       .
