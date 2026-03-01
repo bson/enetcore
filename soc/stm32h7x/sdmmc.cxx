a2977001393938795115124a12349ae291087baf
Jan Brittenson <bson@rockgarden.net>
2026-02-28 17:43:39 -0800

    add SDMMC impl to H753

---
+++ soc/stm32h7x/sdmmc.cxx
@@ -0,0 +1,261 @@
+//
+// Copyright 2026 Jan Brittenson
+// See LICENSE for details. 
+//
+#include <stdint.h>
+
+#include "core/enetcore.h"
+#include "core/bits.h"
+#include "core/bitfield.h"
+
+#include "soc/stm32h7x/sdmmc.h"
+
+
+// Cache flush
+static void cache_clean(const void *addr, uint32_t len)
+{
+    //SCB_CleanDCache_by_Addr((uint32_t*)addr, len);
+}
+
+// Cache invalidation
+static void cache_invalidate(void *addr, uint32_t len)
+{
+    //SCB_InvalidateDCache_by_Addr((uint32_t*)addr, len);
+}
+
+
+// Note: device needs to be enabled before use
+
+static uint32_t get_kernel_clk()
+{
+    return AHB_FREQ;
+}
+
+
+template <uintptr_t SDMMC>
+Stm32Sdio<SDMMC>::Stm32Sdio()
+    : _state(State::IDLE), _result(0)
+{
+    reg(Register::MASKR) = Bitfield(0)
+        .bit(CMDRENDIE)
+        .bit(CMDSENTIE)
+        .bit(CTIMEOUTIE)
+        .bit(CCRCFAILIE)
+        .bit(DATAENDIE)
+        .bit(DTIMEOUTIE)
+        .bit(DCRCFAILIE);
+}
+
+
+template <uintptr_t SDMMC>
+void Stm32Sdio<SDMMC>::set_clock(uint32_t hz)
+{
+    uint32_t div = (get_kernel_clk() / hz / 2);
+    if (div > 0)
+        div -= 1;
+
+    assert(div <= 0x3ff);
+
+    reg(Register::CLKCR) = Bitfield(reg(Register::CLKCR))
+        .f(CLKDIV_W, CLKDIV, div);
+}
+
+
+template <uintptr_t SDMMC>
+void Stm32Sdio<SDMMC>::set_bus_width_1bit()
+{
+    reg(Register::CLKCR) = Bitfield(reg(Register::CLKCR))
+        .f(WIDBUS_W, WIDBUS, Widbus::SingleBit);
+}
+
+
+template <uintptr_t SDMMC>
+void Stm32Sdio<SDMMC>::set_bus_width_4bit()
+{
+    reg(Register::CLKCR) = Bitfield(reg(Register::CLKCR))
+        .f(WIDBUS_W, WIDBUS, Widbus::FourBit);
+}
+
+
+template <uintptr_t SDMMC>
+typename Stm32Sdio<SDMMC>::Result
+Stm32Sdio<SDMMC>::send_cmd(uint8_t cmd,
+                               uint32_t arg,
+                               uint32_t resp_type,
+                               uint32_t *response)
+{
+    while (_state != State::IDLE)
+        Thread::WaitFor(this);
+
+    // XXX This should be done atomically in the wait, above
+    _state = State::CMD_IN_PROGRESS;
+    _result = Result::SUCCESS;
+
+    reg(Register::ICR) = 0xffffffff;
+    reg(Register::ARGR) = arg;
+
+    uint32_t cmdreg = Bitfield(0)
+        .f(CMDINDEX_W, CMDINDEX, cmd)
+        .bit(CPSMEN);
+
+    if (resp_type == 2)
+        cmdreg = Bitfield(cmdreg).f(WAITRESP_W, WAITRESP, 2);
+    else if (resp_type != 0)
+        cmdreg = Bitfield(cmdreg).f(WAITRESP_W, WAITRESP, 1);
+
+    reg(Register::CMDR) = cmdreg;
+
+    while (_state != State::DONE)
+        Thread::WaitFor(this);
+
+    _state = State::IDLE;
+    Thread::WakeSingle(this);
+
+    return _result;
+}
+
+
+template <uintptr_t SDMMC>
+typename Stm32Sdio<SDMMC>::Result
+Stm32Sdio<SDMMC>::data_read(void *buf, uint32_t bytes)
+{
+    // Buffer must be aligned on 32-byte boundary
+    assert(((uintptr_t)buf & 31) == 0);
+
+    while (_state != State::IDLE)
+        Thread::WaitFor(this);
+
+    cache_invalidate(buf, bytes);
+
+    // XXX This should be done atomically in the wait, above
+    _state = State::DATA_IN_PROGRESS;
+    _result = Result::SUCCESS;
+
+    reg(Register::DTIMER) = 0xffffffff;
+    reg(Register::DLEN)   = bytes;
+
+    reg(Register::DMABASE0) = (uintptr_t)buf;
+    reg(Register::IDMACTRL) = BIT(IDMAEN);
+
+    reg(Register::DCTRL) = Bitfield(0)
+        .bit(DTEN)
+        .bit(DTDIR)
+        .f(DBLOCKSIZE_W, DBLOCKSIZE, 9); // 512-byte block
+
+    while (_state != State::DONE)
+        Thread::WaitFor(this);
+
+    cache_invalidate(buf, bytes);
+
+    _state = State::IDLE;
+    Thread::WakeSingle(this);
+
+    return _result;
+}
+
+
+template <uintptr_t SDMMC>
+typename Stm32Sdio<SDMMC>::Result
+Stm32Sdio<SDMMC>::data_write(const void *buf, uint32_t bytes)
+{
+    // Buffer must be aligned on 32-byte boundary
+    assert(((uintptr_t)buf & 31) == 0);
+
+    while (_state != State::IDLE)
+        Thread::WaitFor(this);
+
+    cache_clean(buf, bytes);
+
+    // XXX This should be done atomically in the wait, above
+    _state = State::DATA_IN_PROGRESS;
+    _result = Result::SUCCESS;
+
+    reg(Register::DTIMER) = 0xffffffff;
+    reg(Register::DLEN)   = bytes;
+
+    reg(Register::IDMABASE0) = (uintptr_t)buf;
+    reg(Register::IDMACTRL)  = BIT(IDMAEN);
+
+    reg(Register::DCTRL) = Bitfield(0)
+        .bit(DTEN)
+        .f(DBLOCKSIZE_W, DBLOCKSIZE, 9); // 512-byte block
+
+    while (_state != State::DONE)
+        Thread::WaitFor(this);
+
+    _state = State::IDLE;
+    Thread::WakeSingle(this);
+
+    return _result;
+}
+
+
+template <uintptr_t SDMMC>
+bool Stm32Sdio<SDMMC>::get_data_crc_error()
+{
+    return reg(Register::STAR) & BIT(DCRCFAIL);
+}
+
+
+template <uintptr_t SDMMC>
+bool Stm32Sdio<SDMMC>::get_data_timeout()
+{
+    return reg(Register::STAR) & BIT(DTIMEOUT);
+}
+
+
+template <uintptr_t SDMMC>
+void Stm32Sdio<SDMMC>::reset_datapath()
+{
+    reg(Register::DCTRL) = 0;
+    reg(Register::IDMACTRL) = 0;
+    reg(Register::ICR) = 0xffffffff;
+}
+
+
+template <uintptr_t SDMMC>
+void Stm32Sdio<SDMMC>::HandleInterrupt()
+{
+    uint32_t sta = reg(Register::STAR);
+
+    /* Command errors */
+    if (sta & BIT(CTIMEOUT))
+        _result = Result::CMD_TIMEOUT;
+
+    if (sta & BIT(CCRCFAIL))
+        _result = Result::CMD_CRC_FAIL;
+
+    /* Data errors */
+    if (sta & BIT(DTIMEOUT))
+        _result = Result::DATA_TIMEOUT;
+
+    if (sta & BIT(DCRCFAIL))
+        _result = Result::DATA_CRC_FAIL;
+
+    /* Command complete */
+    if (sta & (BIT(CMDREND) | BIT(CMDSENT))) {
+        reg(Register::ICR) = Bitfield(0)
+            .bit(CMDRENDC)
+            .bit(CMDSENTC)
+            .bit(CTIMEOUTC)
+            .bit(CCRCFAILC);
+
+        if (_state == State::CMD_IN_PROGRESS) {
+            _state = State::DONE;
+            Thread::WakeSingle(this);
+        }
+    }
+
+    /* Data complete */
+    if (sta & BIT(DATAEND)) {
+
+        reg(Register::IDMACTRL) = 0;
+        reg(Register::ICR) = Bitfield(0)
+            .bit(DATAENDC)
+            .bit(DTIMEOUTC)
+            .it(DCRCFAILC);
+
+        _state = State::DONE;
+        Thread::WakeSingle(this);
+    }
+}
---
+++ soc/stm32h7x/sdmmc.h
@@ -0,0 +1,252 @@
+//
+// Copyright 2026 Jan Brittenson
+// See LICENSE for details. 
+//
+
+#ifndef __SDMMC_H__
+#define __SDMMC_H__
+
+#include <stdint.h>
+#include "core/sdio.h"
+
+
+// Assume only one SDIO bus, so specialize it.
+template <uintptr_t SDMMC>
+class Stm32Sdio: public Sdio {
+    enum class Register {
+        POWER = 0x000,
+        CLKCR = 0x004,
+        ARGR = 0x008,
+        CMDR = 0x00c,
+        RESPCMDR = 0x010,
+        RESP1R = 0x014,
+        RESP2R = 0x018,
+        RESP3R = 0x01c,
+        RESP4R = 0x020,
+        DTIMER = 0x024,
+        DLENR = 0x028,
+        DCTRLR = 0x02c,
+        DCNTR = 0x030,
+        STAR = 0x034,
+        ICR = 0x038,
+        MASKR = 0x03c,
+        ACKTIMER = 0x040,
+        IDMACTRLR = 0x050,
+        IDMABSIZER = 0x054,
+        IDMABASE0R = 0x058,
+        IDMABASE1R = 0x05c,
+        FIFORx = 0x080,
+    };
+
+    
+    enum {
+        // POWER
+        DIRPOL = 4,
+        VSWITCHEN = 3,
+        VSWITCH = 2,
+        PWRCTL = 0,             // 2b
+        PWRCTL_W = 2,
+
+        // CLKCR
+        SELCLKRX = 20,          // 2b
+        SELCLKRX_W = 2,
+        BUSSPEED = 19,
+        DDR = 18,
+        HWFC_EN = 17,
+        NEGEDGE = 16,
+        WIDBUS = 14,            // 2b
+        WIDBUS_W = 2,
+        PWRSAV = 12,
+        CLKDIV = 0,             // 10b
+        CLKDIV_W = 10,
+
+        // CMDR
+        CMDSUSPEND = 16,
+        BOOTEN = 15,
+        BOOTMODE = 14,
+        DTHOLD = 13,
+        CPSMEN = 12,
+        WAITPEND = 11,
+        WAITINT = 10,
+        WAITRESP = 8,           // 2b
+        WAITRESP_W = 2,
+        CMDSTOP = 7,
+        CMDTRANS = 6,
+        CMDINDEX = 0,           // 6b
+        CMDINDEX_W = 6,
+
+        // RESPCMDR
+        RESPCMD = 0,            // 6b
+        RESPCMD_W = 6,
+
+        // DLENR
+        DATALENGTH = 0,         // 25b
+        DATALENGTH_W = 25,
+
+        // DCTRL
+        FIFORST = 13,
+        BOOTACKEN = 12,
+        SDIOEN = 11,
+        RWMOD = 10,
+        RWSTOP = 9,
+        RWSTART = 8,
+        DBLOCKSIZE = 4,         // 4b
+        DBLOCKSIZE_W = 4,
+        DTMODE = 2,             // 2b
+        DTMODE_W = 2,
+        DTDIR = 1,
+        DTEN = 0,
+
+        // DCNTR
+        DATACOUNT = 0,          // 25b
+        DATACOUNT_W = 25,
+
+        // STAR
+        IDMABTC = 28,
+        IDMATE = 27,
+        CKSTOP = 26,
+        VSWEND = 25,
+        ACKTIMEOUT = 24,
+        ACKFAIL = 23,
+        SDIOIT = 22,
+        BUSYD0END = 21,
+        BUSYD0 = 20,
+        RXFIFOE = 19,
+        TXFIFOE = 18,
+        RXFIFOF = 17,
+        TXFIFOF = 16,
+        RXFIFOHF = 15,
+        TXFIFOHE = 14,
+        CPSMACT = 13,
+        DPSMACT = 12,
+        DABORT = 11,
+        DBCKEND = 10,
+        DHOLD = 9,
+        DATAEND = 8,
+        CMDSENT = 7,
+        CMDREND = 6,
+        RXOVERR = 5,
+        TXUNDERR = 4,
+        DTIMEOUT = 3,
+        CTIMEOUT = 2,
+        DCRCFAIL = 1,
+        CCRCFAIL = 0,
+
+        // ICR
+        IDMABTCC = 28,
+        IDMATEC = 27,
+        CKSTOPC = 26,
+        VSWENDC = 25,
+        ACKTIMEOUTC = 24,
+        ACKFAILC = 23,
+        SDIOITC = 22,
+        BUSYD0ENDC = 21,
+        DABORTC = 11,
+        DBCKENDC = 10,
+        DHOLDC = 9,
+        DATAENDC = 8,
+        CMDSENTC = 7,
+        CMDRENDC = 6,
+        RXOVERRC = 5,
+        TXUNDERRC = 4,
+        DTIMEOUTC = 3,
+        CTIMEOUTC = 2,
+        DCRCFAILC = 1,
+        CCRCFAILC = 0,
+
+        // MASKR
+        IDMABTCE = 28,
+        CKSTOPIE = 26,
+        VSWENDIE = 25,
+        ACKTIMEOUTIE = 24,
+        ACKFAILIE = 23,
+        SDIOITIE = 22,
+        BUSYD0ENDIE = 21,
+        TXFIFOEIE = 18,
+        RXFIFOFIE = 17,
+        RXFIFOHFIE = 15,
+        TXFIFOHEIE = 14,
+        DABORTIE = 11,
+        DBCKENDIE = 10,
+        DHOLDIE = 9,
+        DATAENDIE = 8,
+        CMDSENTIE = 7,
+        CMDRENDIE = 6,
+        RXVERRIE = 5,
+        TXUNDERRIE = 4,
+        DTIMEOUTIE = 3,
+        CTIMEOUTIE = 2,
+        DCRCFAILIE = 1,
+        CCRCFAILIE = 0,
+
+        // ACKTIMER
+        ACKTIME = 0,            // 25b
+        ACTIME_W = 25,
+
+        // IDMACTRLR
+        IDMABACT = 2,
+        IDMABMODE = 1,
+        IDMAEN = 0,
+
+        // IDMABSIZE
+        IDMABNDT = 5,           // 8b
+        IDMABNDT_W = 8,
+    };
+
+
+    enum Widbus {
+        SingleBit = 0b00,
+        FourBit = 0b01,
+        EightBit = 0b10
+    };
+
+    enum class State {
+        IDLE = 0,
+        CMD_IN_PROGRESS,
+        DATA_IN_PROGRESS,
+        DONE
+    };
+
+public:
+    enum Result : int {
+        SUCCESS = 0,
+        CMD_TIMEOUT = -1,
+        CMD_CRC_FAIL = -2,
+        DATA_TIMEOUT = -3,
+        DATA_CRC_FAIL = -4,
+    };
+
+private:
+    State _state;
+    Result _result;
+
+    volatile uint32_t& reg(Register r) { return *(volatile uint32_t*)(SDMMC + (uint32_t)r); }
+
+	// Interrupt handler
+	static void Interrupt(void* token) {
+        ((Stm32Sdio<SDMMC>*)token)->HandleInterrupt();
+    }
+
+public:
+    // NVIC needs to set up for IRQ, with object ref to this
+    Stm32Sdio();
+
+    // Sdio interface implementation
+    void set_clock(uint32_t hz);
+    void set_bus_width_1bit();
+    void set_bus_width_4bit();
+    Result send_cmd(uint8_t cmd, uint32_t arg, uint32_t resp_type, uint32_t *response);
+    Result data_read(void *buf, uint32_t bytes);
+    Result data_write(const void *buf, uint32_t bytes);
+    bool get_data_crc_error();
+    bool get_data_timeout();
+    void reset_datapath();
+
+    // Interrupt handler
+    void HandleInterrupt();
+};
+
+typedef Stm32Sdio<BASE_SDMMC1> Stm32Sdmmc1;
+typedef Stm32Sdio<BASE_SDMMC2> Stm32Sdmmc2;
+
+#endif // __SDMMC_H__
